374cdba328343c210b328cdd414b43ae53965549
Kemal Setya Adhi <dev.kemalsetyaa@gmail.com>
2026-04-24 01:21:22 +0700

    [HSR Game Repair] Fix missing URL on Audio and Video assets

--- CollapseLauncher/Classes/RepairManagement/StarRailV2/StarRailPersistentRefResult.GetPersistentFiles.cs
+++ CollapseLauncher/Classes/RepairManagement/StarRailV2/StarRailPersistentRefResult.GetPersistentFiles.cs
@@ -293,12 +293,13 @@ internal partial class StarRailPersistentRefResult
 
             FilePropertiesRemote file = new()
             {
-                RN            = url,
-                N             = relPathInPersistent,
-                S             = asset.FileSize,
-                CRCArray      = asset.MD5Checksum,
-                FT            = StarRailRepairV2.DetermineFileTypeFromExtension(asset.Filename ?? ""),
-                IsHasHashMark = isHashMarked
+                RN               = url,
+                N                = relPathInPersistent,
+                S                = asset.FileSize,
+                CRCArray         = asset.MD5Checksum,
+                FT               = StarRailRepairV2.DetermineFileTypeFromExtension(asset.Filename ?? ""),
+                IsHasHashMark    = isHashMarked,
+                AssociatedObject = asset
             };
             fileDic.TryAdd(relPathInPersistent, file);
             fileList.Add(file);
